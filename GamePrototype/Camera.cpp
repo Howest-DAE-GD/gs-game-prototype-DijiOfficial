117769db4ce280becd20342a5aede6c4c32319c9
Dylan Burgisser <33450259+DijiOfficial@users.noreply.github.com>
2024-04-21 05:32:59 +0200

    added basic camera framework

---
+++ GamePrototype/Camera.cpp
@@ -0,0 +1,86 @@
+#include "pch.h"
+#include "Scene.h"
+
+Camera::Camera(Scene* scene, const float width, const float height) 
+	: GameObject{ scene }
+	, m_Width{ width }
+	, m_Height{ height }
+	, m_LevelBoundaries{ 0.f, 0.f, width, height }
+	, m_CameraOffset{ 0 }
+	, m_IsLocked{ false }
+{
+}
+
+//void Camera::SetLevelBoundaries(const Rectf& levelBoundaries)
+//{
+//	m_LevelBoundaries = levelBoundaries;
+//}
+
+Point2f Camera::Track(const Rectf& target) const
+{
+	return Point2f{ (target.left + target.width / 2) - m_Width / 2, (target.bottom + target.height / 2) - m_Height / 2 };
+}
+
+void Camera::Clamp(Point2f& bottomLeftPos) const
+{
+	if (bottomLeftPos.x < m_LevelBoundaries.left) bottomLeftPos.x = m_LevelBoundaries.left + 1.f;
+	if (bottomLeftPos.x + m_Width - m_CameraOffset > m_LevelBoundaries.left + m_LevelBoundaries.width or m_IsLocked)
+	{
+		bottomLeftPos.x = m_LevelBoundaries.left + m_LevelBoundaries.width - m_Width - m_CameraOffset;
+	}
+
+	if (bottomLeftPos.y < m_LevelBoundaries.bottom) bottomLeftPos.y = m_LevelBoundaries.bottom + 1.f;
+	if (bottomLeftPos.y + m_Height > m_LevelBoundaries.bottom + m_LevelBoundaries.height)
+	{
+		bottomLeftPos.y = m_LevelBoundaries.height + m_LevelBoundaries.bottom - m_Height;
+	}
+}
+
+void Camera::Render()
+{
+	Transform(GetOwner()->GetGameObject<Player>()->GetShape());
+}
+
+void Camera::Transform(const Rectf& target) const
+{
+	Point2f CameraPos{ Track(target) };
+	Clamp(CameraPos);
+	glTranslatef(-CameraPos.x, 0.f, 0);
+}
+
+//bool Camera::GetCameraLock() const
+//{
+//	return m_IsLocked;
+//}
+//
+//void Camera::UnlockCamera()
+//{
+//	m_IsLocked = false;
+//}
+//
+//void Camera::LockCamera()
+//{
+//	m_IsLocked = true;
+//}
+//
+//Rectf Camera::GetCameraPos(const Rectf& target) const
+//{
+//	Point2f CameraPos{ Track(target) };
+//	Clamp(CameraPos);
+//	return Rectf{ CameraPos.x, CameraPos.y, m_Width, m_Height };
+//}
+//
+//void Camera::OffsetCamera(const int offset)
+//{
+//	m_CameraOffset -= offset;
+//}
+//
+//void Camera::ResetOffset()
+//{
+//	m_CameraOffset = 0;
+//}
+//
+//int Camera::GetCameraOffset() const
+//{
+//	return m_CameraOffset;
+//}
---
+++ GamePrototype/Camera.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "GameObject.h"
+
+class Camera final : public GameObject
+{
+public:
+	Camera(Scene* scene, float width, float height);
+
+	Camera(const Camera& other) = delete;
+	Camera(Camera&& other) = delete;
+	Camera& operator=(const Camera& other) = delete;
+	Camera& operator=(Camera&& other) = delete;
+	
+	void Update() override {};
+	void Render() override;
+	//void SetLevelBoundaries(const Rectf& levelBoundaries);
+	void Transform(const Rectf& target) const;
+	//Rectf GetCameraPos(const Rectf& target) const;
+	//bool GetCameraLock() const;
+	//void UnlockCamera();
+	//void LockCamera();
+	//void OffsetCamera(const int offset);
+	//void ResetOffset();
+	//int GetCameraOffset() const;
+
+private:
+	float m_Width;
+	float m_Height;
+	Rectf m_LevelBoundaries;
+	int m_CameraOffset{ 0 };
+	bool m_IsLocked{ 0 };
+
+	Point2f Track(const Rectf& target) const;
+	void Clamp(Point2f& bottomLeftPos) const;
+};
+
